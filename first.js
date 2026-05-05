98bede8f5f72384ae7f5559abb0f0d7c6ed6b914
afreenagnhub <afreen.agnhub@gmail.com>
2026-05-06 01:06:32 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,151 @@
+// document.write("Hiiiii" + "<br>");
+// console.log("Hi javascript");
+// alert("HEYYY");
+
+// Variables - name, name12, first_name, nameFirst, name-first
+// no special characters in variable
+// 12name, &name  => X
+// a = "Wasib";
+// document.write(a +"<br>");
+// document.write("<strong>" + a + "</strong> " +"<br>");
+
+// Data-type => number, string, boolean, float, array, object
+// string = ""
+// number = 123
+// boolean = true/false
+// float = 1.23
+// array = []
+// object = {"key":"value", "key":123}
+
+// Operators - symbols
+// 1.Arithmetic op=> +,-,*,/,%,**,++,--
+// a = 10;
+// b = 3;
+// document.write(a+b+"<br>");
+// document.write(a%b);
+// document.write(a**b +"<br>");
+// document.write(a-- +"<br>");
+
+// 2.Assignment op => +=, -=,*=,=,/=,%=,**=
+// a = 4;
+// b = 6;
+// document.write(a+=b);
+// a+=b => a = a+b
+
+//3. Comparison Operator => ==, ===, !=, >, <, >=, <=
+// document.write(a == b);
+// document.write(!a);
+// != => not equal
+
+// 4. Logical operator => &&, ||, !
+// && => both value true
+// || => one true one false
+// ! => not equal 
+
+//5. Ternary operator => condition?"true":"false"
+
+// Variables => let, var, const
+
+// var a = "Wasib";
+// var => variable, a => variable name, "Wasib"=> variable value
+// a = "Qureshi";
+// document.write(a + "<br>");
+
+// redeclaration & reassignment 
+// var b = "xyz";
+// var b = "mno";  
+// redeclaration
+// document.write(b+"<br>");
+
+// let m = "abc";
+// m = "def";
+// document.write(m);     reassignment
+
+// let n = "abc";
+// let n = "def";      //no redeclaration
+// document.write(n);
+
+// const x = "mango";
+// document.write(x);
+// const => no redeclaration and no reassignment
+
+// if else - 
+// if(condition){
+//     statement => output
+// }
+// else{
+//     statement
+// }
+
+// let age = 18;
+// if(age>=18){
+//     console.log("Eligible");
+// }
+// else{
+//     console.log("Not eligible");
+// }
+
+// Nested if else -
+// if(condition){
+//     output
+// }
+// else if(condition){
+//     output
+// }
+// else if(condition){
+//     output
+// }
+// else{
+//     output
+// }
+
+// let age = 25;
+// let hasId = false;
+// if(age>=18 || hasId){
+//     console.log("eligible")
+// }
+// else{
+//     console.log("deny");
+// }
+
+// let islogin = true;
+// if(!islogin){
+//     console.log("Please login");
+// }
+// else{
+//     console.log("welcome");
+// }
+
+// let marks = 85;
+// let attendance = 80;
+// if(marks >= 90 && attendance>=75){
+//     console.log("EXCELL");
+// }
+// else if(marks >= 70 && attendance >=60){
+//     console.log("Good");
+// }
+// else if(marks >= 50 || attendance >=70){
+//     console.log("Average");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+// marks => basis => grades (A,B,C,D)
+
+
+// ternary operator =>
+// let age = 1;
+// let result = (age>=18) ? "You can vote": "not";
+// console.log(result);
+
+
+// nested ternary operATOR=>
+// let marks = 5;
+// let grade = (marks>=90)?"A":(marks>=70)?"B":(marks>=50)?"C":"fail";
+// console.log(grade);
+
+
+
+
+
