05d31ed14930b97751bfa3b66546ee6be410c85f
afreenagnhub <afreen.agnhub@gmail.com>
2026-05-12 01:22:54 +0530

    12May2026

--- first.js
+++ first.js
@@ -147,5 +147,137 @@
 
 
 
+// function -  reusable
+
+// function func_name(){
+//     document.write("output");
+// }
+// func_name()
+
+// function names(){
+//     document.write("wasib");
+// }
+// names();
+
+// function with parameter - 
+// function names(parameter1, parameter2){
+//     return parameter
+// }
+// names(argument1, argument2)
+
+// function names(para){
+//     return para;
+// }
+// document.write(names("wasib"));
+
+// function names(num,num2){
+//     return num+num2;
+// }
+// document.write(names(3,5));
+
+// while loop - 
+// 1.initialization
+// 2.condition
+// 3.increment/decrement
+
+// let i = 1;
+// while(condition){
+//     print 
+//     i++/i--;
+// }
+
+
+// odd-
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i += 2;   
+//     //i = i+2
+// }
+
+// reverse -
+// let i = 10;
+// while(i >= 1){
+//     console.log(i);
+//     i--;   
+
+// }
+
+// let i = 1;
+// while(i <= 10){
+//     document.write("wasib" +"<br>");
+//     i++;
+// }
+
+// print even no from 1-20 & print odd no , reverse countdown
+
+
+// while loop with break -
+// let i = 1;
+// while(i<=10){
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// while loop with continue - current iteration skip and move to next
+// let i = 0;
+// while(i<=10){
+//     i++;
+//     if(i===5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// for loop - 
+// for(let i = 1;i<=10;i++){
+//     console.log(i)
+// };
+
+// for(let i = 1;i<=10;i++){
+//     console.log("Wasib");
+// };
+
+// nested loop - 
+// for(let i = 1;i<=10;i++){   //outer loop OR row
+//     for(let j = 1;j<=10;j++){  //inner loop or Column
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+// for(let i = 1;i<=10;i++){   
+//     for(let j=i;j<=10;j++){  
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+
+// pyramid - 
+for(let i=1;i<=5;i++){  //row
+    let pattern = "";
+    // spaces-
+    for(let j=1;j<=(5-i);j++){  //column + space
+        pattern += " ";
+    }
+    // stars -
+    for(let k=1;k<=(2*i-1);k++){  //star pattern OR number pattern
+        pattern += "*";
+    }
+    console.log(pattern);
+}
+
+
+
+
+
+
+
+
 
 
